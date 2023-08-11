e771ced916bce2c60b3a9eda7b8995ed7918f307
Usman Nasution <usmannasty32@gmail.com>
2023-08-11 19:26:41 +0700

    Updated web.js. Added strg and dstrg functions to set item in localStorage

--- src/web.js
+++ src/web.js
@@ -1,4 +1,5 @@
 export default {
+  tab : null,
   render : () => 0,
   navigate(tab){
     this.set('tab', tab);
@@ -7,5 +8,14 @@ export default {
     this[key] = value;
     this.render();
   },
-  tab : null,
+  strg(key, value){
+    if(value === undefined)
+      return window.localStorage.getItem(key);
+    window.localStorage.setItem(key, value);
+    this.render();
+  },
+  dstrg(key){
+    window.localStorage.removeItem(key);
+    this.render();
+  }
 }
